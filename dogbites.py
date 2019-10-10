88b60cac454cd47cf2ac6f5458cf7f1b5b94312f
Danny Phu <54083130+dannyphu@users.noreply.github.com>
2019-10-10 00:13:37 -0400

    Create dogbites.py

---
+++ dogbites.py
@@ -0,0 +1,46 @@
+"""
+Dog_bites.py
+
+Reported number of dog bites in NYC from 2015 to 2017.
+
+"""
+
+import sys
+import urllib.request
+import csv
+import collections
+
+#https://data.cityofnewyork.us/Health/DOHMH-Dog-Bite-Data/rsgh-akpg
+url = "https://data.cityofnewyork.us/resource/rsgh-akpg.json"
+
+try:
+    urlfile = urllib.request.urlopen(url)
+except urllib.error.URLError as error:
+    print(error, file = sys.stderr)
+    sys.exit(1)
+
+readbytes = urlfile.read() #reads whole file into one big sequence of bytes
+urlfile.close()
+
+try:
+    string1 = readbytes.decode("utf-8")
+except UnicodeError as unicodeError:
+    print(unicodeError, file = sys.stderr)
+    sys.exit(1)
+
+lines =csv.reader(string1.splitlines()) #list of lists
+
+
+createtuple = [
+    line[8]
+    for line in lines
+    if "DOG" in line[2]
+    ]
+
+print("Count for number of dog bites in each borough from 2015-2017:")
+
+for borough, n in collections.Counter(createtuple).most_common(5):
+    print(f"{borough}: {n}")
+
+
+sys.exit(0)
